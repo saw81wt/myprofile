dd481fd6bd178e2967fefadb3281439eb0947af5
前原宗太朗 <maeharasoutarou@maeharasoutarounoMacBook-Air.local>
2019-04-28 19:51:25 +0900

    not use vue-router

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,18 @@
     <!-- <router-link to="/">Home</router-link> |
     <router-link to="/skills">Skills</router-link> | 
     <router-link to="/sns">SNS</router-link> -->
-    <router-view />
+    <Home />
   </div>
 </template>
 
 
 <script>
+import Home from '@/views/Home'
 
 export default {
+  components:{
+    Home
+  },
   methods: {
     goHome: function(){
       this.$router.push({path: "/"})
@@ -28,17 +32,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,17 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+// import router from './router'
 
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { far } from '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(fas, fab, far)
+library.add(fas)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
@@ -21,7 +19,6 @@ Vue.use(VueMaterial)
 Vue.config.productionTip = false
 
 new Vue({
-  router,
   render: h => h(App)
 }).$mount('#app')
 
